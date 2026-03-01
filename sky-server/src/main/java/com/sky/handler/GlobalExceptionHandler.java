72ba308bb8e3de334dcb5f16b265ead1d45e3048
BM22333 <3522332803@qq.com>
2026-03-01 18:01:55 +0800

    新增员工业务代码

--- sky-server/src/main/java/com/sky/handler/GlobalExceptionHandler.java
+++ sky-server/src/main/java/com/sky/handler/GlobalExceptionHandler.java
@@ -1,10 +1,14 @@
 package com.sky.handler;
 
+import com.sky.constant.MessageConstant;
 import com.sky.exception.BaseException;
 import com.sky.result.Result;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.sql.SQLIntegrityConstraintViolationException;
+
 
 /**
  * 全局异常处理器，处理项目中抛出的业务异常
@@ -22,6 +26,24 @@ public class GlobalExceptionHandler {
     public Result exceptionHandler(BaseException ex){
         log.error("异常信息：{}", ex.getMessage());
         return Result.error(ex.getMessage());
+    }
+
+    /**
+     * 处理SQL语句异常
+     * @param ex
+     * @return
+     */
+    @ExceptionHandler
+    public Result exceptionHandler(SQLIntegrityConstraintViolationException ex){
+        String exMessage = ex.getMessage();
+
+        if (exMessage.contains(ex.getMessage())) {
+            // 用户名已存在
+            String msg = MessageConstant.USERNAME + MessageConstant.ALREADY_EXIST;
+            return Result.error(msg);
+        }
+
+        return Result.error(MessageConstant.UNKNOWN_ERROR);
     }
 
 }
